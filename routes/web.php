d7dca42c43d29d483e346801f83a5c470caeb676
ahlaiptek <iptekahla@gmail.com>
2025-12-01 16:56:34 +0700

    Implement HomeController with index method and update home route to use controller

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,11 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return redirect('/dashboard');
-})->name('home');
-
+Route::controller(App\Http\Controllers\HomeController::class)
+    ->group(function () {
+        Route::get('/', 'index')->name('home');
+    });
+    
 Route::prefix('/dashboard/meets')
     ->controller(UserMeetController::class)
     ->group(function() {
