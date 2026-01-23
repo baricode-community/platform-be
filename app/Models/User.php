7becd2a8c3e4b621787a58b907a159fc35a405b3
ahlaiptek <iptekahla@gmail.com>
2026-01-23 15:34:18 +0700

    filament 5

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,7 +14,7 @@ use App\Models\Fun\Meme;
 use App\Models\Fun\MemeVote;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable, HasRoles;
@@ -110,4 +112,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(DailyCommitTracker::class);
     }
+
+     public function canAccessPanel(Panel $panel): bool
+     {
+        return true;
+     }
 }
