ee82f1d8487a608d7c30d70a9f3ed6291f541d8c
ahlaiptek <iptekahla@gmail.com>
2026-02-01 16:31:53 +0700

    feat: Include username in user creation validation and model population

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -20,6 +20,12 @@ class CreateNewUser implements CreatesNewUsers
     {
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
+            'username' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique(User::class),
+            ],
             'email' => [
                 'required',
                 'string',
@@ -33,6 +39,7 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'username' => $input['username'],
             'password' => $input['password'],
         ]);
     }
