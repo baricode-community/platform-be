f7876dba212062873498faf54f2778271078f15a
ahlaiptek <iptekahla@gmail.com>
2026-02-01 16:22:45 +0700

    refactor: Remove background code snippets and footer from base layout

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -6,25 +6,8 @@
 </head>
 
 <body>
-
-    <!-- Background Code Snippets -->
-    <div class="fixed inset-0 opacity-5 pointer-events-none">
-        <pre class="text-purple-300 text-sm">
-function learn() {
-    const skills = ['HTML', 'CSS', 'JS'];
-    return skills.map(skill => master(skill));
-}
-            </pre>
-    </div>
     
     {{ $slot }}
-
-    <!-- Footer -->
-    <footer class="py-12 px-4 border-t border-white/10">
-        <div class="max-w-7xl mx-auto text-center text-gray-400">
-            <p class="mb-4">© {{ date('Y') }} Baricode Community. Made with 💜 by Indonesian Developers</p>
-        </div>
-    </footer>
 
     @fluxScripts
 </body>
