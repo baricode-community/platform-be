b27e39356b0924e0cc9f4446696aafeba8c9ff71
ahlaiptek <iptekahla@gmail.com>
2026-02-03 16:49:18 +0700

    feat: Implement Course management resource with CRUD functionality

---
+++ app/Filament/Resources/Courses/Schemas/CourseForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Courses\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class CourseForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('slug')
+                    ->required(),
+                TextInput::make('duration')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                Toggle::make('is_published')
+                    ->required(),
+            ]);
+    }
+}
