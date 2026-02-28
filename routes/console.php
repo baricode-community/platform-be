008fb063e3b61448293128dd77cf07dc8ad1e155
ahlaiptek <iptekahla@gmail.com>
2026-03-01 00:30:09 +0700

    feat: Add daily backup cleanup command at midnight

--- routes/console.php
+++ routes/console.php
@@ -10,4 +10,4 @@ Artisan::command('inspire', function () {
 Schedule::command('backup:run')->daily()->at('01:30');
 Schedule::command('backup:run')->daily()->at('09:30');
 Schedule::command('backup:run')->daily()->at('17:30');
-
+Schedule::command('backup:cleanup')->daily()->at('00:00');
