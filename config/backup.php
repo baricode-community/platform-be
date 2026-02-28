8b6ace607f97b6f690917aea43c01efb2ad6eba4
ahlaiptek <iptekahla@gmail.com>
2026-03-01 00:08:06 +0700

    feat: Change backup disk from S3 to local for monitoring configuration

--- config/backup.php
+++ config/backup.php
@@ -260,7 +260,7 @@ return [
     'monitor_backups' => [
         [
             'name' => env('APP_NAME', 'laravel-backup'),
-            'disks' => ['s3'],
+            'disks' => ['local'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumStorageInMegabytes::class => 5000,
