b7c40954d7bec33ea3e3bcac1c42d8f0c84015b2
ahlaiptek <iptekahla@gmail.com>
2026-03-01 00:23:10 +0700

    feat: Add daily backup schedule commands at specified times

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('backup:run')->daily()->at('01:30');
+Schedule::command('backup:run')->daily()->at('09:30');
+Schedule::command('backup:run')->daily()->at('17:30');
+
