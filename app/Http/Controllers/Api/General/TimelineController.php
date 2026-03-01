9d7adbdbea0d66d86f51b2b50cd0b334d5f7e871
ahlaiptek <iptekahla@gmail.com>
2026-03-02 00:28:28 +0700

    feat: Remove unused controllers and implement TimelineController for timeline management

---
+++ app/Http/Controllers/Api/General/TimelineController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\General;
+
+use App\Http\Controllers\Controller;
+use App\Models\Timeline;
+use Illuminate\Http\Request;
+
+class TimelineController extends Controller
+{
+    /**
+     * Display a listing of the timelines.
+     */
+    public function index(Request $request)
+    {
+        $query = Timeline::query();
+
+        // Filter by status if provided
+        if ($request->filled('status')) {
+            $query->where('status', $request->status);
+        }
+
+        $timelines = $query
+            ->orderByDesc('created_at')
+            ->paginate(10);
+
+        return response()->json($timelines);
+    }
+
+    /**
+     * Display the specified timeline.
+     */
+    public function show(Timeline $timeline)
+    {
+        return response()->json($timeline);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,28 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Note: HomeController is not yet implemented
-// Route::controller(App\Http\Controllers\HomeController::class)
-//     ->group(function () {
-//         Route::get('/', 'index')->name('home');
-//     });
-
-// Note: UserController is not yet implemented
-// Route::controller(App\Http\Controllers\UserController::class)
-//     ->group(function () {
-//         Route::get('/', 'index')->name('users.index');
-//         Route::get('/list', 'list')->name('users.list');
-//         Route::get('/{user:username}', 'show')->name('users.show');
-//     });
-
-// Note: MeetController is not yet implemented
-// Route::controller(App\Http\Controllers\MeetController::class)
-//     ->prefix('meets')
-//     ->group(function () {
-//         Route::get('/', 'index')->name('meets.index');
-//         Route::get('/list', 'list')->name('meets.list');
-//     });
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// Timeline Routes
+Route::controller(App\Http\Controllers\Api\General\TimelineController::class)
+    ->prefix('timelines')
+    ->group(function () {
+        Route::get('/', 'index')->name('timelines.index');
+        Route::get('/{timeline}', 'show')->name('timelines.show');
+    });
