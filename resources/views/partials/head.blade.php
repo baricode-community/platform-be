63f9216dd2f897895a8a9af8dd6edf1c1f018329
ahlaiptek <iptekahla@gmail.com>
2026-03-08 13:01:10 +0700

    feat: Add tracking script for analytics in head partial

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -11,6 +11,7 @@
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<script defer src="https://app.pushaja.com/pixel/CqNnkAudQLcIs0JU"></script>
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
